d043cd01a57b221ee638a02169ccb614569439b4
Matthew Hudes <Matthewhudes@gmail.com>
2026-08-20 10:48:47 -0700

    Old changes on gigan

--- src/incflo_apply_predictor.cpp
+++ src/incflo_apply_predictor.cpp
@@ -237,9 +237,12 @@ void incflo::ApplyPredictor (StepType step_type, bool incremental_projection)
     // **********************************************************************************************
     // Project velocity field, update pressure
     // **********************************************************************************************
-    // ApplyProjection(get_density_nph_const(),
-    //                 AMREX_D_DECL(GetVecOfPtrs(u_mac), GetVecOfPtrs(v_mac),
-    //                 GetVecOfPtrs(w_mac)),new_time,m_dt,incremental_projection);
+    if (uses_RK3_timestepping()) {
+        incremental_projection = true;
+    }
+    ApplyProjection(get_density_nph_const(),
+                    AMREX_D_DECL(GetVecOfPtrs(u_mac), GetVecOfPtrs(v_mac),
+                    GetVecOfPtrs(w_mac)),new_time,m_dt,incremental_projection);
 
 #ifdef INCFLO_USE_PARTICLES
     // **************************************************************************************
