020296849541f16f007355015219f6754760a72e
Matthew Hudes <Matthewhudes@gmail.com>
2026-08-24 15:10:14 -0700

    Removed mac projection of stochastic fluxes:

--- src/incflo_stochastic.cpp
+++ src/incflo_stochastic.cpp
@@ -214,36 +214,6 @@ incflo::compute_stochastic_velocity_force_RK3 (Vector<MultiFab const*> const& de
                                   beta[0], stochastic_flux_B[dir], 0, 0, AMREX_SPACEDIM, 0);
                 flux_ptrs[dir] = &stochastic_flux_RK3_one[dir];
             }
-            macproj_stochastic_flux.reset();
-
-            Vector<Array<MultiFab, AMREX_SPACEDIM> > flux_alias(1);
-            Vector<Array<MultiFab*, AMREX_SPACEDIM> > mac_vec(1);
-            Vector<Geometry> stochastic_geom(1, geom[lev]);
-            LPInfo lp_info;
-            lp_info.setMaxCoarseningLevel(m_mac_mg_max_coarsening_level);
-
-            Array<LinOpBCType, AMREX_SPACEDIM> periodic_bc;
-            periodic_bc.fill(LinOpBCType::Periodic);
-
-            macproj_stochastic_flux =
-                std::make_unique<Hydro::MacProjector>(stochastic_geom);
-            Vector<BoxArray> ba(1);
-            Vector<DistributionMapping> dm(1);
-            ba[0] = stochastic_flux_RK3_one[0].boxArray();
-            dm[0] = stochastic_flux_RK3_one[0].DistributionMap();
-            macproj_stochastic_flux->initProjector(ba, dm, lp_info, Real(1.0));
-            macproj_stochastic_flux->setDomainBC(periodic_bc, periodic_bc);
-
-            for (int n = 0; n < AMREX_SPACEDIM; ++n) {
-                for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
-                    flux_alias[0][dir] =
-                        MultiFab(stochastic_flux_RK3_one[dir], amrex::make_alias, n, 1);
-                    mac_vec[0][dir] = &flux_alias[0][dir];
-                }
-
-                macproj_stochastic_flux->setUMAC(mac_vec);
-                macproj_stochastic_flux->project(m_mac_mg_rtol, m_mac_mg_atol);
-            }
 
             amrex::computeDivergence(m_stochastic_vel_force_RK3_stage_one[lev], flux_ptrs, geom[lev]);
 
@@ -263,36 +233,6 @@ incflo::compute_stochastic_velocity_force_RK3 (Vector<MultiFab const*> const& de
                                   beta[1], stochastic_flux_B[dir], 0, 0, AMREX_SPACEDIM, 0);
                 flux_ptrs[dir] = &stochastic_flux_RK3_two[dir];
             }
-            macproj_stochastic_flux.reset();
-
-            Vector<Array<MultiFab, AMREX_SPACEDIM> > flux_alias(1);
-            Vector<Array<MultiFab*, AMREX_SPACEDIM> > mac_vec(1);
-            Vector<Geometry> stochastic_geom(1, geom[lev]);
-            LPInfo lp_info;
-            lp_info.setMaxCoarseningLevel(m_mac_mg_max_coarsening_level);
-
-            Array<LinOpBCType, AMREX_SPACEDIM> periodic_bc;
-            periodic_bc.fill(LinOpBCType::Periodic);
-
-            macproj_stochastic_flux =
-                std::make_unique<Hydro::MacProjector>(stochastic_geom);
-            Vector<BoxArray> ba(1);
-            Vector<DistributionMapping> dm(1);
-            ba[0] = stochastic_flux_RK3_two[0].boxArray();
-            dm[0] = stochastic_flux_RK3_two[0].DistributionMap();
-            macproj_stochastic_flux->initProjector(ba, dm, lp_info, Real(1.0));
-            macproj_stochastic_flux->setDomainBC(periodic_bc, periodic_bc);
-
-            for (int n = 0; n < AMREX_SPACEDIM; ++n) {
-                for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
-                    flux_alias[0][dir] =
-                        MultiFab(stochastic_flux_RK3_two[dir], amrex::make_alias, n, 1);
-                    mac_vec[0][dir] = &flux_alias[0][dir];
-                }
-
-                macproj_stochastic_flux->setUMAC(mac_vec);
-                macproj_stochastic_flux->project(m_mac_mg_rtol, m_mac_mg_atol);
-            }
 
             amrex::computeDivergence(m_stochastic_vel_force_RK3_stage_two[lev], flux_ptrs, geom[lev]);
             const Real rhoinv = Real(1.0) / m_ro_0;
@@ -310,36 +250,6 @@ incflo::compute_stochastic_velocity_force_RK3 (Vector<MultiFab const*> const& de
                 MultiFab::LinComb(stochastic_flux_RK3_three[dir], Real(1.0), stochastic_flux_A[dir], 0,
                                   beta[2], stochastic_flux_B[dir], 0, 0, AMREX_SPACEDIM, 0);
                 flux_ptrs[dir] = &stochastic_flux_RK3_three[dir];
-            }
-            macproj_stochastic_flux.reset();
-
-            Vector<Array<MultiFab, AMREX_SPACEDIM> > flux_alias(1);
-            Vector<Array<MultiFab*, AMREX_SPACEDIM> > mac_vec(1);
-            Vector<Geometry> stochastic_geom(1, geom[lev]);
-            LPInfo lp_info;
-            lp_info.setMaxCoarseningLevel(m_mac_mg_max_coarsening_level);
-
-            Array<LinOpBCType, AMREX_SPACEDIM> periodic_bc;
-            periodic_bc.fill(LinOpBCType::Periodic);
-
-            macproj_stochastic_flux =
-                std::make_unique<Hydro::MacProjector>(stochastic_geom);
-            Vector<BoxArray> ba(1);
-            Vector<DistributionMapping> dm(1);
-            ba[0] = stochastic_flux_RK3_three[0].boxArray();
-            dm[0] = stochastic_flux_RK3_three[0].DistributionMap();
-            macproj_stochastic_flux->initProjector(ba, dm, lp_info, Real(1.0));
-            macproj_stochastic_flux->setDomainBC(periodic_bc, periodic_bc);
-
-            for (int n = 0; n < AMREX_SPACEDIM; ++n) {
-                for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
-                    flux_alias[0][dir] =
-                        MultiFab(stochastic_flux_RK3_three[dir], amrex::make_alias, n, 1);
-                    mac_vec[0][dir] = &flux_alias[0][dir];
-                }
-
-                macproj_stochastic_flux->setUMAC(mac_vec);
-                macproj_stochastic_flux->project(m_mac_mg_rtol, m_mac_mg_atol);
             }
 
             amrex::computeDivergence(m_stochastic_vel_force_RK3_stage_three[lev], flux_ptrs, geom[lev]);
